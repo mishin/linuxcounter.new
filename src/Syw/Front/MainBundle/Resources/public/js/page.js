4a4afd42458090c530f05c573b1c58c986e023a2
alexloehner <alex.loehner@linux.com>
2015-03-17 11:34:10 +0100

    more changes for the new linuxcounter symfony test

--- src/Syw/Front/MainBundle/Resources/public/js/page.js
+++ src/Syw/Front/MainBundle/Resources/public/js/page.js
@@ -3,10 +3,10 @@ $(function(){
     $(window).scroll(function(event){
         var st = $(this).scrollTop();
         if (st > lastScroll) {
-            $("#navwrapper").animate({'margin-top': "0px"}, {queue: false, duration: 1000});
+            $("#navwrapper").animate({'margin-top': "0px"}, {queue: false, duration: 500});
         } else {
             if (st <= 10) {
-                $("#navwrapper").animate({'margin-top': "20px"}, {queue: false, duration: 1000});
+                $("#navwrapper").animate({'margin-top': "20px"}, {queue: false, duration: 500});
             }
         }
         lastScroll = st;
