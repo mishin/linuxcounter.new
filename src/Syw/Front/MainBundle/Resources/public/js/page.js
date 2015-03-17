cbc151a4f282757f3d04793a7de4e739ad7f71db
alexloehner <alex.loehner@linux.com>
2015-03-17 12:38:39 +0100

    final layout finished

--- src/Syw/Front/MainBundle/Resources/public/js/page.js
+++ src/Syw/Front/MainBundle/Resources/public/js/page.js
@@ -1,19 +1,3 @@
-$(function(){
-    var lastScroll = 0;
-    $(window).scroll(function(event){
-        var st = $(this).scrollTop();
-        if (st > lastScroll) {
-            $("#navwrapper").animate({'margin-top': "0px"}, {queue: false, duration: 500});
-        } else {
-            if (st <= 10) {
-                $("#navwrapper").animate({'margin-top': "20px"}, {queue: false, duration: 500});
-            }
-        }
-        lastScroll = st;
-    });
-});
-
-
 
 
 
