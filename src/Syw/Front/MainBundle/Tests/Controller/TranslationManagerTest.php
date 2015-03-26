8328f2b348c480127e563db618fc60e278452a31
alexloehner <alex.loehner@linux.com>
2015-03-26 10:17:09 +0100

    translation tests fixes

--- src/Syw/Front/MainBundle/Tests/Controller/TranslationManagerTest.php
+++ src/Syw/Front/MainBundle/Tests/Controller/TranslationManagerTest.php
@@ -6,14 +6,14 @@ class TranslationManagerTest extends BaseControllerTest
 {
     public function testGetAllTranslations()
     {
-        $trans = $this->_em->getRepository('SywFrontMainBundle:Translation')->findAll();
+        $trans = $this->_em->getRepository('AsmTranslationLoaderBundle:Translation')->findAll();
         // At least 2000 translations must be existent in the database
         $this->assertGreaterThan(1999, sizeof($trans));
     }
 
     public function testGetAllMessagesTranslations()
     {
-        $trans = $this->_em->getRepository('SywFrontMainBundle:Translation')->findAll(array('messageDomain' => 'messages'));
+        $trans = $this->_em->getRepository('AsmTranslationLoaderBundle:Translation')->findAll(array('messageDomain' => 'messages'));
         // At least 12 translations must be existent in the database
         $this->assertGreaterThan(11, sizeof($trans));
     }
