6769bd3062a11cda60d3ee766408aa2cd4646de1
alexloehner <alex.loehner@linux.com>
2015-03-28 21:18:30 +0100

    added contact form

--- src/Syw/Front/MainBundle/Controller/MainController.php
+++ src/Syw/Front/MainBundle/Controller/MainController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Syw\Front\MainBundle\Form\Type\ContactType;
 
 class MainController extends BaseController
 {
@@ -33,11 +34,10 @@ class MainController extends BaseController
 
     /**
      * @Route("/contact")
-     * @Method("GET")
      *
      * @Template()
      */
-    public function contactAction()
+    public function contactAction(Request $request)
     {
         $languages = $this->get('doctrine')
             ->getRepository('SywFrontMainBundle:Languages')
@@ -48,7 +48,41 @@ class MainController extends BaseController
         } else {
             $user = null;
         }
-        return array('languages' => $languages, 'user' => $user);
+
+        $form = $this->createForm(new ContactType());
+
+        if ($request->isMethod('POST')) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $message = \Swift_Message::newInstance()
+                    ->setSubject($form->get('subject')->getData())
+                    ->setFrom($form->get('email')->getData())
+                    ->setTo($this->container->getParameter('admin_email'))
+                    ->setBody(
+                        $this->renderView(
+                            'SywFrontMainBundle:Mail:contact.html.twig',
+                            array(
+                                'ip' => $request->getClientIp(),
+                                'name' => $form->get('name')->getData(),
+                                'message' => $form->get('message')->getData()
+                            )
+                        )
+                    );
+
+                $this->get('mailer')->send($message);
+
+                $request->getSession()->getFlashBag()->add('success', 'Your email has been sent! Thanks!');
+
+                return $this->redirect($this->generateUrl('syw_front_main_main_contact'));
+            }
+        }
+
+        return array(
+            'form' => $form->createView(),
+            'languages' => $languages,
+            'user' => $user
+        );
     }
 
     /**
---
+++ src/Syw/Front/MainBundle/Form/Type/ContactType.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Syw\Front\MainBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Collection;
+
+/**
+ * Class ContactType
+ *
+ * @category FormType
+ * @package  SywFrontMainBundle
+ * @author   Alexander Löhner <alex.loehner@linux.com>
+ * @license  GPL v3
+ * @link     https://github.com/alexloehner/linuxcounter.new
+ */
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text', array(
+                'attr' => array(
+                    'placeholder' => 'What\'s your name?',
+                    'pattern'     => '.{2,}' //minlength
+                )
+            ))
+            ->add('email', 'email', array(
+                'attr' => array(
+                    'placeholder' => 'So I can get back to you.'
+                )
+            ))
+            ->add('subject', 'text', array(
+                'attr' => array(
+                    'placeholder' => 'The subject of your message.',
+                    'pattern'     => '.{3,}' //minlength
+                )
+            ))
+            ->add('message', 'textarea', array(
+                'attr' => array(
+                    'cols' => 90,
+                    'rows' => 10,
+                    'placeholder' => 'Your message text...'
+                )
+            ));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $collectionConstraint = new Collection(array(
+            'name' => array(
+                new NotBlank(array('message' => 'Name should not be blank.')),
+                new Length(array('min' => 2))
+            ),
+            'email' => array(
+                new NotBlank(array('message' => 'Email should not be blank.')),
+                new Email(array('message' => 'Invalid email address.'))
+            ),
+            'subject' => array(
+                new NotBlank(array('message' => 'Subject should not be blank.')),
+                new Length(array('min' => 3))
+            ),
+            'message' => array(
+                new NotBlank(array('message' => 'Message should not be blank.')),
+                new Length(array('min' => 5))
+            )
+        ));
+
+        $resolver->setDefaults(array(
+            'constraints' => $collectionConstraint
+        ));
+    }
+
+    public function getName()
+    {
+        return 'contact';
+    }
+}
