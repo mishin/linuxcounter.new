016b3dfb6ab5827b2ddb9b255a26596a857713c1
alexloehner <alex.loehner@linux.com>
2015-03-29 20:38:22 +0200

    access rights and much more... also added scrutinizer and highcharts

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,6 +29,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             // new MktCode\CaitchaBundle\MktcodeCaitchaBundle(),
+            new Ob\HighchartsBundle\ObHighchartsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
