98605c9cbac54489ef78b68fd760ca6b3489eb40
alexloehner <alex.loehner@linux.com>
2015-03-29 13:22:25 +0200

    files bug

--- src/Syw/Front/MainBundle/Command/ImportFromLicoCommand.php
+++ src/Syw/Front/MainBundle/Command/ImportFromLicoCommand.php
@@ -294,10 +294,11 @@ EOT
                 unset($licotest);
 
                 $z++;
-                $mypid = getmypid();
 
-                $files = @exec('lsof -p '.$mypid.' | wc -l');
-                file_put_contents("import.log", ">>> ".$counter." | ".$z." | ".$id." |   open files: ".$files." | Memory info: ".number_format(round((memory_get_usage()/1000), 2))." Mb   (".number_format(round((memory_get_peak_usage()/1000), 2))." Mb) \n", FILE_APPEND);
+                // $mypid = getmypid();
+                // $files = @exec('lsof -p '.$mypid.' | wc -l');
+                // file_put_contents("import.log", ">>> ".$counter." | ".$z." | ".$id." |   open files: ".$files." | Memory info: ".number_format(round((memory_get_usage()/1000), 2))." Mb   (".number_format(round((memory_get_peak_usage()/1000), 2))." Mb) \n", FILE_APPEND);
+                file_put_contents("import.log", ">>> ".$counter." | ".$z." | ".$id." \n", FILE_APPEND);
 
                 gc_collect_cycles();
             }
